ebc090d853ee42eab0e39570956434a02dae01fa
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-12-02 16:37:44 +0100

    Day2 part1
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,12 @@
 const std = @import("std");
 const day1 = @import("day1.zig");
+const day2 = @import("day2.zig");
 
 pub fn main() u8 {
     std.log.info("Welcome to Advent of Code 2021", .{});
 
     day1.solve();
+    day2.solve();
 
     return 0;
 }
