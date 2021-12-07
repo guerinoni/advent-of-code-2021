172804b9d0d52d790775f1fd06d4a339d723fa2f
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-12-07 13:09:53 +0100

    Day7 part1
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,7 @@ const day3 = @import("day3.zig");
 const day4 = @import("day4.zig");
 const day5 = @import("day5.zig");
 const day6 = @import("day6.zig");
+const day7 = @import("day7.zig");
 
 pub fn main() !void {
     std.log.info("Welcome to Advent of Code 2021", .{});
@@ -15,4 +16,5 @@ pub fn main() !void {
     try day4.solve();
     try day5.solve();
     try day6.solve();
+    try day7.solve();
 }
