888861b5c5fc59831e48d3c4df3002e6c060c89e
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-12-07 21:14:11 +0100

    Day7 part2
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/day7.zig
+++ src/day7.zig
@@ -43,12 +43,11 @@ pub fn solve() !void {
     }
 
     var crabs = nums.toOwnedSlice();
-
-    std.log.info("Day7 \n\tpart 1 -> {}\n\tpart 2 -> {}", .{part1(crabs), part2()});
+    std.sort.sort(i64, crabs, {}, comptime std.sort.asc(i64));
+    std.log.info("Day7 \n\tpart 1 -> {}\n\tpart 2 -> {}", .{part1(crabs), part2(crabs)});
 }
 
 fn part1(positions: []i64) !i64 {
-    std.sort.sort(i64, positions, {}, comptime std.sort.asc(i64));
     const median = positions[positions.len / 2];
     var fuel: i64 = 0;
     for (positions) |pos| {
@@ -58,8 +57,40 @@ fn part1(positions: []i64) !i64 {
     return fuel;
 }
 
-fn part2() !u64 {
-    return 0;
+// --- Part Two ---
+// The crabs don't seem interested in your proposed solution. Perhaps you misunderstand crab engineering?
+
+// As it turns out, crab submarine engines don't burn fuel at a constant rate. Instead, each change of 1 step in horizontal position costs 1 more unit of fuel than the last: the first step costs 1, the second step costs 2, the third step costs 3, and so on.
+
+// As each crab moves, moving further becomes more expensive. This changes the best horizontal position to align them all on; in the example above, this becomes 5:
+
+// Move from 16 to 5: 66 fuel
+// Move from 1 to 5: 10 fuel
+// Move from 2 to 5: 6 fuel
+// Move from 0 to 5: 15 fuel
+// Move from 4 to 5: 1 fuel
+// Move from 2 to 5: 6 fuel
+// Move from 7 to 5: 3 fuel
+// Move from 1 to 5: 10 fuel
+// Move from 2 to 5: 6 fuel
+// Move from 14 to 5: 45 fuel
+// This costs a total of 168 fuel. This is the new cheapest possible outcome; the old alignment position (2) now costs 206 fuel instead.
+
+// Determine the horizontal position that the crabs can align to using the least fuel possible so they can make you an escape route! How much fuel must they spend to align to that position?
+
+fn part2(positions: []i64) !i64 {
+    var sum: i64 = 0;
+    for (positions) |pos| {
+        sum += pos;
+    }
+
+    var avg = @divFloor(sum, @intCast(i64, positions.len));
+    var fuel: i64 = 0;
+    for (positions) |pos| {
+        var diff = std.math.absInt(pos - avg) catch unreachable;
+        fuel += @divTrunc(diff * (diff + 1), 2);
+    }
+    return fuel;
 }
 
 test "part1 test" {
