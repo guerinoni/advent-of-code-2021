8eb9f8fc0a2ac411eec6a47b424ed87f9a39892c
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-12-10 12:32:18 +0100

    Day10 part2
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/day10.zig
+++ src/day10.zig
@@ -100,6 +100,90 @@ fn part1() !u64 {
     return total;
 }
 
+// --- Part Two ---
+// Now, discard the corrupted lines. The remaining lines are incomplete.
+
+// Incomplete lines don't have any incorrect characters - instead, they're missing some closing characters at the end of the line. To repair the navigation subsystem, you just need to figure out the sequence of closing characters that complete all open chunks in the line.
+
+// You can only use closing characters (), ], }, or >), and you must add them in the correct order so that only legal pairs are formed and all chunks end up closed.
+
+// In the example above, there are five incomplete lines:
+
+// [({(<(())[]>[[{[]{<()<>> - Complete by adding }}]])})].
+// [(()[<>])]({[<{<<[]>>( - Complete by adding )}>]}).
+// (((({<>}<{<{<>}{[]{[]{} - Complete by adding }}>}>)))).
+// {<[[]]>}<{[{[{[]{()[[[] - Complete by adding ]]}}]}]}>.
+// <{([{{}}[<[[[<>{}]]]>[]] - Complete by adding ])}>.
+// Did you know that autocomplete tools also have contests? It's true! The score is determined by considering the completion string character-by-character. Start with a total score of 0. Then, for each character, multiply the total score by 5 and then increase the total score by the point value given for the character in the following table:
+
+// ): 1 point.
+// ]: 2 points.
+// }: 3 points.
+// >: 4 points.
+// So, the last completion string above - ])}> - would be scored as follows:
+
+// Start with a total score of 0.
+// Multiply the total score by 5 to get 0, then add the value of ] (2) to get a new total score of 2.
+// Multiply the total score by 5 to get 10, then add the value of ) (1) to get a new total score of 11.
+// Multiply the total score by 5 to get 55, then add the value of } (3) to get a new total score of 58.
+// Multiply the total score by 5 to get 290, then add the value of > (4) to get a new total score of 294.
+// The five lines' completion strings have total scores as follows:
+
+// }}]])})] - 288957 total points.
+// )}>]}) - 5566 total points.
+// }}>}>)))) - 1480781 total points.
+// ]]}}]}]}> - 995444 total points.
+// ])}> - 294 total points.
+// Autocomplete tools are an odd bunch: the winner is found by sorting all of the scores and then taking the middle score. (There will always be an odd number of scores to consider.) In this example, the middle score is 288957 because there are the same number of scores smaller and larger than it.
+
+// Find the completion string for each incomplete line, score the completion strings, and sort the scores. What is the middle score?
+
+fn score_of_(close: u8) !u64 {
+    return switch (close) {
+        ')' => 1,
+        ']' => 2,
+        '}' => 3,
+        '>' => 4,
+        else => unreachable,
+    };
+}
+
 fn part2() !u64 {
-    return 0;
+    var lines = std.mem.tokenize(input, "\n");
+    var stack = std.ArrayList(u8).init(std.testing.allocator);
+    defer stack.deinit();
+
+    var scores = std.ArrayList(u64).init(std.testing.allocator);
+    defer scores.deinit();
+
+    while (lines.next()) | line | {
+        var corrupted = false;
+        for (line) | ch | {
+            switch (ch) {
+                '(', '[', '{', '<' => try stack.append(ch),
+                else => {
+                    const last = stack.pop();
+                    if ((try reverse_of(last)) != ch) {
+                        corrupted = true;
+                        stack.shrinkRetainingCapacity(0);
+                        break;
+                    }
+                },
+            }
+        }
+
+        if (!corrupted) {
+            var total : u64 = 0;
+            while (stack.items.len != 0) {
+                const last = stack.pop();
+                total *= 5;
+                total += try score_of_(try reverse_of(last));
+            }
+
+            try scores.append(total);
+        }
+    }
+
+    std.sort.sort(u64, scores.items, {}, comptime std.sort.asc(u64));
+    return scores.items[scores.items.len / 2];
 }
