d47ad77ac1714644159fbe4852772c1998c8787d
Federico Guerinoni <guerinoni.federico@gmail.com>
2022-01-07 22:22:35 +0100

    Day17 part2
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/day17.zig
+++ src/day17.zig
@@ -134,7 +134,7 @@ pub fn solve() !void {
 
     _ = target;
 
-    std.log.info("Day17 \n\tpart 1 -> {}\n\tpart 2 -> {}", .{ part1(target), part2() });
+    std.log.info("Day17 \n\tpart 1 -> {}\n\tpart 2 -> {}", .{ part1(target), part2(target) });
 }
 
 const Probe = struct {
@@ -175,17 +175,14 @@ const Probe = struct {
 };
 
 fn part1(target: Target) !i32 {
- var x: i32 = 1;
+    var x: i32 = 1;
     var y: i32 = -100;
     var max_y: i32 = std.math.minInt(i32);
-    var valid = std.ArrayList(Probe).init(std.testing.allocator);
 
     while (true) {
         var probe = Probe { .x = 0, .y = 0, .vel_x = x, .vel_y = y };
         if (probe.sim(target)) |max| {
             if (max > max_y) max_y = max;
-
-            try valid.append(probe);
         }
 
         x += 1;
@@ -200,6 +197,49 @@ fn part1(target: Target) !i32 {
     return max_y;
 }
 
-fn part2() !u32 {
-    return 0;
+// --- Part Two ---
+// 
+// Maybe a fancy trick shot isn't the best idea; after all, you only have one probe, so you had better not miss.
+// 
+// To get the best idea of what your options are for launching the probe, you need to find every initial velocity that causes the probe to eventually be within the target area after any step.
+// 
+// In the above example, there are 112 different initial velocity values that meet these criteria:
+// 
+// 23,-10  25,-9   27,-5   29,-6   22,-6   21,-7   9,0     27,-7   24,-5
+// 25,-7   26,-6   25,-5   6,8     11,-2   20,-5   29,-10  6,3     28,-7
+// 8,0     30,-6   29,-8   20,-10  6,7     6,4     6,1     14,-4   21,-6
+// 26,-10  7,-1    7,7     8,-1    21,-9   6,2     20,-7   30,-10  14,-3
+// 20,-8   13,-2   7,3     28,-8   29,-9   15,-3   22,-5   26,-8   25,-8
+// 25,-6   15,-4   9,-2    15,-2   12,-2   28,-9   12,-3   24,-6   23,-7
+// 25,-10  7,8     11,-3   26,-7   7,1     23,-9   6,0     22,-10  27,-6
+// 8,1     22,-8   13,-4   7,6     28,-6   11,-4   12,-4   26,-9   7,4
+// 24,-10  23,-8   30,-8   7,0     9,-1    10,-1   26,-5   22,-9   6,5
+// 7,5     23,-6   28,-10  10,-2   11,-1   20,-9   14,-2   29,-7   13,-3
+// 23,-5   24,-8   27,-9   30,-7   28,-5   21,-10  7,9     6,6     21,-5
+// 27,-10  7,2     30,-9   21,-8   22,-7   24,-9   20,-6   6,9     29,-5
+// 8,-2    27,-8   30,-5   24,-7
+// 
+// How many distinct initial velocity values cause the probe to be within the target area after any step?
+
+fn part2(target: Target) !usize {
+    var x: i32 = 1;
+    var y: i32 = -100;
+    var valid = std.ArrayList(Probe).init(std.testing.allocator);
+
+    while (true) {
+        var probe = Probe { .x = 0, .y = 0, .vel_x = x, .vel_y = y };
+        if (probe.sim(target)) |_| {
+            try valid.append(probe);
+        }
+
+        x += 1;
+        if (x > target.x_max) {
+            x = 1;
+            y += 1;
+        }
+
+        if (probe.x_at_max > target.x_max and y > 1000) break;
+    }
+
+    return valid.items.len;
 }
